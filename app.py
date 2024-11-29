048c4d92d010d09f04c787b29b90cba725e5bee8
malerivera <a20211490@pucp.edu.pe>
2024-11-28 21:08:32 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# Título de la app
+st.title("¡Hola!")
+st.write("Mi nombre es Maria Alejandra Rivera.")
+
+# Imagen (si está en tu repo)
+st.image("IMG_5224.JPG", caption="Esta soy yo")
